55028b95eae8577ebb00a6978639bbf565527b4a
user86 <user86@user86>
2017-07-28 10:01:45 +0900

    0728 10:00

---
+++ javaPrj/src/wrapping/Program.java
@@ -0,0 +1,18 @@
+package wrapping;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//new Integer(3).intValue();
+		//int a = 3.intValue();
+		
+		//String s = new String("Hello").length();
+		
+		//String f = "hello".length();
+
+		int n = 3;
+		String s= n;
+		
+	}
+
+}
